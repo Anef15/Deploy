b6a6053010003b3076a2c75350dad4cefda197c6
Anef15 <service.informatique@anef15.fr>
2026-08-12 15:50:54 +0200

    Update setTasks.ps1

--- setTasks.ps1
+++ setTasks.ps1
@@ -1,4 +1,5 @@
 $dossier = "C:\IT\Deploy-main\tasks"
-foreach ($script in Get-ChildItem $dossier -Filter "*.ps1" -File) {
-    & $script.FullPath
+
+foreach ($script in Get-ChildItem -Path $dossier -Filter "*.ps1" -File) {
+    & powershell.exe -ExecutionPolicy Bypass -File $script.FullName
 }
